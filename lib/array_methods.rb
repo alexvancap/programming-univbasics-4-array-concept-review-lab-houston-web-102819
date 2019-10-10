20d885cc3f7fcc08a7d2de22527905e2aac61c3d
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-10 00:14:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -23,8 +23,9 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  counter = array.length
+  counter = array.size
   min_value = array[0]
+  
   while array[counter] do
     if array[counter] < min_value
       min_value = array[counter]

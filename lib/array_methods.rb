3b4c431dcd7d3584c9ddcbf784ad1f2d321a4dfc
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-10 00:11:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -26,7 +26,7 @@ def find_min_value(array)
   counter = 1
   min_value = array[0]
   while array[counter] do
-    if array[counter] < min_value
-      return
+    if array[counter] <= min_value
+      min_value = array[counter]
     end
 end
